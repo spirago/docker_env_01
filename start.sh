ccd3ee27a42f0138bb70f774e0c79b115c3d1bb2
petcebch <144560619+petcebch@users.noreply.github.com>
2023-10-03 13:42:38 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,15 +2,14 @@
 
 echo "pod started"
 
-if [[ $PUBLIC_KEY ]]
-then
-    mkdir -p ~/.ssh
-    chmod 700 ~/.ssh
-    cd ~/.ssh
-    echo $PUBLIC_KEY >> authorized_keys
-    chmod 700 -R ~/.ssh
-    cd /
-    service ssh start
+# Setup ssh
+setup_ssh() {
+    if [[ $PUBLIC_KEY ]]; then
+        echo "Setting up SSH..."
+        mkdir -p ~/.ssh
+        echo "$PUBLIC_KEY" >> ~/.ssh/authorized_keys
+        chmod 700 -R ~/.ssh
+        service ssh start
 fi
 conda init bash
 echo "conda activate ludwig" >> ~/.bashrc
