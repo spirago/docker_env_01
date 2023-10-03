e9c618de652809ceb9deab85e59fed21d6c55447
petcebch <144560619+petcebch@users.noreply.github.com>
2023-10-03 14:18:45 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-set -e  # Exit the script if any statement returns a non-true return value
 
 echo "pod started"
 
-# Setup ssh
-setup_ssh() {
-    if [[ $PUBLIC_KEY ]]; then
-        echo "Setting up SSH..."
-        mkdir -p ~/.ssh
-        echo "$PUBLIC_KEY" >> ~/.ssh/authorized_keys
-        chmod 700 -R ~/.ssh
-        service ssh start
-}
+if [[ $PUBLIC_KEY ]]
+then
+    mkdir -p ~/.ssh
+    chmod 700 ~/.ssh
+    cd ~/.ssh
+    echo $PUBLIC_KEY >> authorized_keys
+    chmod 700 -R ~/.ssh
+    cd /
+    service ssh start
 fi
-setup_ssh
 conda init bash
 echo "conda activate ludwig" >> ~/.bashrc
 source /root/.bashrc
-conda activate ludwig
-echo "Start script(s) finished, pod is ready to use."
-sleep infinity 
+# python /ludwig_finetune.py
+sleep infinity
